ce235bf71a7b79009ac58d83dd5d6d39ce3e64c7
gervickasjs <wellitogervickas@gmail.com>
2024-06-26 22:56:16 -0300

    fix: loop

--- src/libs/api/user.ts
+++ src/libs/api/user.ts
@@ -2,30 +2,32 @@ import { User } from '@/types/payload-types';
 import { get } from './payload';
 
 export const getUser = async (cookies?: string): Promise<User | null> => {
-  if (typeof window === `undefined` && cookies) {
-    const result = await get(
-      `/users/me`,
-      {},
-      {
-        headers: {
-          Cookie: cookies,
-        },
-        cache: `no-store`,
-      },
-    );
+  // if (typeof window === `undefined` && cookies) {
+  const result = await get(
+    `/users/me`,
+    {},
+    {
+      headers: cookies
+        ? {
+            Cookie: cookies,
+          }
+        : {},
+      cache: `no-store`,
+    },
+  );
 
-    return result?.user || null;
-  } else {
-    try {
-      const result = await fetch(
-        `${process.env.NEXT_PUBLIC_SITE_URL}/api/users/me/`,
-      );
-      const { user } = await result.json();
+  return result?.user || null;
+  // } else {
+  //   try {
+  //     const result = await fetch(
+  //       `${process.env.NEXT_PUBLIC_SITE_URL}/api/users/me/`,
+  //     );
+  //     const { user } = await result.json();
 
-      return user;
-    } catch (e) {
-      console.log(`Error getting user data.`, e);
-      return null;
-    }
-  }
+  //     return user;
+  //   } catch (e) {
+  //     console.log(`Error getting user data.`, e);
+  //     return null;
+  //   }
+  // }
 };
