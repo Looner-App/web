e4f2b88dfd5d87585f0f8beb05dc489173cad658
gervickasjs <wellitogervickas@gmail.com>
2024-06-26 23:01:44 -0300

    fix: header as ssr

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -11,7 +11,7 @@ import { Points } from '@/types/payload-types';
 import dynamic from 'next/dynamic';
 
 const WalletButton = dynamic(() => import(`../wallet/Button`), {
-  ssr: false,
+  ssr: true,
   loading: () => (
     <div className="bg-azure-blue text-white transition hocustive:bg-white hocustive:text-black rounded-lg font-semibold md:py-3 py-2 md:px-6 px-4 disabled:opacity-50 text-sm md:text-lg flex w-fit items-center space-x-2">
       Loading ...
