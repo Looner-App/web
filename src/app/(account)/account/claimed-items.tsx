b38ad0adc61686630be3eb13ce0406c5f4e8afb4
Indra Gunanda <indra.gunanda@gmail.com>
2024-10-09 08:40:44 +0700

    fix(ui): filter out coin & mapbox integration

--- src/app/(account)/account/claimed-items.tsx
+++ src/app/(account)/account/claimed-items.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ImagePayload, IImagePayload } from '@/components/image';
+import { IImagePayload, ImagePayload } from '@/components/image';
 import { Points } from '@/types/payload-types';
 import { format } from 'date-fns';
 import Image from 'next/image';
@@ -43,46 +43,48 @@ export const ClaimedItems = ({ data }: IClaimedItems) => {
       </div>
 
       <ul className="grid divide-y grid-cols-1 border border-zinc-700 divide-zinc-700 text-sm">
-        {data.items.map((item, _i) => (
-          <li key={_i} className="py-3 flex items-center">
-            <div className="px-4 flex-shrink hidden md:block">
-              <div className="w-10 h-10 bg-zinc-100/10 overflow-hidden rounded">
-                {typeof item.image === `object` && (
-                  <a
-                    href={String(item.image.url)}
-                    target="_blank"
-                    title="thumbnail"
-                  >
-                    <ImagePayload
-                      src={item.image}
-                      size="thumbnail"
-                      blur={false}
-                      width={40}
-                      height={40}
-                    />
-                  </a>
-                )}
+        {data.items
+          .filter((e) => e.category !== `Coins`)
+          .map((item, _i) => (
+            <li key={_i} className="py-3 flex items-center">
+              <div className="px-4 flex-shrink hidden md:block">
+                <div className="w-10 h-10 bg-zinc-100/10 overflow-hidden rounded">
+                  {typeof item.image === `object` && (
+                    <a
+                      href={String(item.image.url)}
+                      target="_blank"
+                      title="thumbnail"
+                    >
+                      <ImagePayload
+                        src={item.image}
+                        size="thumbnail"
+                        blur={false}
+                        width={40}
+                        height={40}
+                      />
+                    </a>
+                  )}
+                </div>
               </div>
-            </div>
 
-            <div className="px-4 flex-1 font-bold">{item.title}</div>
+              <div className="px-4 flex-1 font-bold">{item.title}</div>
 
-            <div className="px-4 flex-1 md:block hidden">{item.category}</div>
+              <div className="px-4 flex-1 md:block hidden">{item.category}</div>
 
-            <div className="px-4 flex-1 md:hidden">{item.shortCategory}</div>
+              <div className="px-4 flex-1 md:hidden">{item.shortCategory}</div>
 
-            <div className="px-4 flex-1">
-              {format(String(item.claimedAt), `MM.dd.yy KK:mmaaa`)}
-            </div>
+              <div className="px-4 flex-1">
+                {format(String(item.claimedAt), `MM.dd.yy KK:mmaaa`)}
+              </div>
 
-            <div className="px-4 flex space-x-4 items-center">
-              <span>
-                <Image src="/point.svg" alt="point" width={20} height={20} />
-              </span>
-              <span>{findClaimedItemPoint(item.id)}</span>
-            </div>
-          </li>
-        ))}
+              <div className="px-4 flex space-x-4 items-center">
+                <span>
+                  <Image src="/point.svg" alt="point" width={20} height={20} />
+                </span>
+                <span>{findClaimedItemPoint(item.id)}</span>
+              </div>
+            </li>
+          ))}
       </ul>
     </div>
   );
