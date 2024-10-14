f6695bfd4f6f47dae7ed05a1e3326f27e9d2dc79
gervickasjs <wellitogervickas@gmail.com>
2024-10-14 12:11:24 -0300

    feat: partial impl basename

--- src/libs/web3/chains/chains.ts
+++ src/libs/web3/chains/chains.ts
@@ -1,13 +1,23 @@
 import { baseSepolia, base } from 'wagmi/chains';
 import { merge } from 'lodash';
 import { Chain } from './types';
+import {
+  BASENAME_RESOLVER_ADDRESS,
+  BASE_SEPOLIA_BASENAME_RESOLVER_ADDRESS,
+} from 'thirdweb/extensions/ens';
 
 export type { Chain };
 
 export const baseSepoliaChain: Chain = merge(baseSepolia, {
   /// insert custom fields here
+  custom: {
+    resolverAddress: BASE_SEPOLIA_BASENAME_RESOLVER_ADDRESS,
+  },
 });
 
 export const baseChain: Chain = merge(base, {
   /// insert custom fields here
+  custom: {
+    resolverAddress: BASENAME_RESOLVER_ADDRESS,
+  },
 });
--- src/libs/web3/thirdweb/components/SignInButton.tsx
+++ src/libs/web3/thirdweb/components/SignInButton.tsx
@@ -1,13 +1,20 @@
 'use client';
 
-import { ConnectButton, useActiveWallet } from 'thirdweb/react';
+import {
+  ConnectButton,
+  useActiveWallet,
+  useEnsName,
+  useSocialProfiles,
+} from 'thirdweb/react';
 import { client } from '@/libs/web3/thirdweb/client';
 import { wallets } from '@/libs/web3/thirdweb/wallets';
 import { useRouter } from 'next/navigation';
 import { toEllipsis } from '@/libs/helper';
-import { thirdwebChains } from '../../chains/config';
+import { thirdwebChains, allowedChainsConfig } from '../../chains/config';
 import './signin-button.css';
 import dynamic from 'next/dynamic';
+import { useEffect, useMemo, useState } from 'react';
+import { resolveL2Name } from 'thirdweb/extensions/ens';
 
 const Button = dynamic(() => import(`@/components/button`), {
   ssr: false,
@@ -15,9 +22,60 @@ const Button = dynamic(() => import(`@/components/button`), {
 const clientInstance = client({
   clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
 });
+
 export const SignInButton = () => {
+  const [name, setName] = useState(``);
   const router = useRouter();
   const activeWallet = useActiveWallet();
+
+  const { data: ens } = useEnsName({
+    client: clientInstance,
+    address: activeWallet?.getAccount()?.address,
+  });
+
+  const { data: profiles } = useSocialProfiles({
+    client: clientInstance,
+    address: activeWallet?.getAccount()?.address,
+  });
+
+  const walletName = useMemo(() => {
+    return (
+      name ||
+      ens ||
+      profiles?.[0]?.name ||
+      toEllipsis(activeWallet?.getAccount()?.address, 6, 6) ||
+      `Connect wallet`
+    );
+  }, [name, ens, profiles, activeWallet]);
+
+  useEffect(() => {
+    const getL2Basename = async () => {
+      if (!activeWallet) return;
+      const address = activeWallet?.getAccount()?.address;
+      if (!address) return;
+
+      const chain = activeWallet.getChain();
+      if (!chain) return;
+
+      const chainCustom = allowedChainsConfig[chain.id];
+      if (!chainCustom) return;
+
+      return await resolveL2Name({
+        client: clientInstance,
+        address,
+        resolverAddress: chainCustom?.custom
+          ?.resolverAddress as unknown as string,
+        resolverChain: chain,
+      });
+    };
+
+    getL2Basename().then((result) => {
+      if (result) {
+        setName(result);
+      }
+    });
+  });
+
   return (
     <Button type="default" className="button-wallet">
       <ConnectButton
@@ -34,15 +92,24 @@ export const SignInButton = () => {
         }}
         detailsButton={{
           style: {
-            background: `transparent !important`,
+            padding: `0px !important`,
+            margin: `0px !important`,
           },
-          render: () => (
-            <button type="button">
-              {toEllipsis(activeWallet?.getAccount()?.address, 6, 6) ||
-                `Connect Wallet`}
-            </button>
-          ),
+          render() {
+            return <>{walletName}</>;
+          },
         }}
+        // detailsButton={{
+        //   style: {
+        //     background: `transparent !important`,
+        //   },
+        //   render: () => (
+        //     <button type="button">
+        //       {toEllipsis(activeWallet?.getAccount()?.address, 6, 6) ||
+        //         `Connect Wallet`}
+        //     </button>
+        //   ),
+        // }}
         signInButton={{
           label: `Sign In`,
         }}
